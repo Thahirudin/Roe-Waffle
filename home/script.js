4af303548c8c9049d918b054aa844497408d3a18
Thahirudin <tohiruzain098@gmail.com>
2023-10-02 13:20:30 +0700

    update home

--- home/script.js
+++ home/script.js
@@ -1,5 +1,33 @@
 $(document).ready(function () {
-  var owl = $(".owl-carousel").owlCarousel({
+  // Inisialisasi Carousel
+  var owl = initializeCarousel();
+
+  // Menghubungkan tombol prev dan next ke slider
+  $(".custom-prev").click(function () {
+    owl.trigger("prev.owl.carousel");
+  });
+
+  $(".custom-next").click(function () {
+    owl.trigger("next.owl.carousel");
+  });
+
+  // Menampilkan indikator carousel
+  var totalSlides = getTotalSlides();
+  createCustomIndicators(totalSlides);
+
+  // Mendeteksi perubahan slide
+  var carousel = document.getElementById("carouselExampleIndicators");
+  carousel.addEventListener("slid.bs.carousel", handleSlideChange);
+
+  // Menampilkan preloader saat dokumen dimuat
+  showPreloader();
+
+  // Setelah 3 detik, sembunyikan preloader
+  setTimeout(hidePreloader, 3000);
+});
+
+function initializeCarousel() {
+  return $(".owl-carousel").owlCarousel({
     loop: true,
     margin: 10,
     responsive: {
@@ -14,42 +42,27 @@ $(document).ready(function () {
       },
     },
   });
-
-  // Menghubungkan tombol prev dan next ke slider
-  $(".custom-prev").click(function () {
-    owl.trigger("prev.owl.carousel");
-  });
-
-  $(".custom-next").click(function () {
-    owl.trigger("next.owl.carousel");
-  });
-});
-// Menentukan jumlah slide
-var totalSlides = document.querySelectorAll(
-  ".carousel-inner .carousel-item"
-).length;
-
-// Membuat indikator secara dinamis
-var customIndicators = document.getElementById("custom-carousel-indicators");
-for (var i = 0; i < totalSlides; i++) {
-  var indicator = document.createElement("span");
-  indicator.className = "custom-indicator";
-  indicator.setAttribute("data-bs-slide-to", i);
-  if (i === 0) {
-    indicator.classList.add("active");
-  }
-  indicator.onclick = function () {
-    var slideNumber = this.getAttribute("data-bs-slide-to");
-    var carousel = document.getElementById("carouselExampleIndicators");
-    var carouselInstance = new bootstrap.Carousel(carousel);
-    carouselInstance.to(parseInt(slideNumber));
-  };
-  customIndicators.appendChild(indicator);
 }
 
-// Mendeteksi perubahan slide
-var carousel = document.getElementById("carouselExampleIndicators");
-carousel.addEventListener("slid.bs.carousel", function () {
+function getTotalSlides() {
+  return document.querySelectorAll(".carousel-inner .carousel-item").length;
+}
+
+function createCustomIndicators(totalSlides) {
+  var customIndicators = document.getElementById("custom-carousel-indicators");
+  for (var i = 0; i < totalSlides; i++) {
+    var indicator = document.createElement("span");
+    indicator.className = "custom-indicator";
+    indicator.setAttribute("data-bs-slide-to", i);
+    if (i === 0) {
+      indicator.classList.add("active");
+    }
+    indicator.onclick = handleIndicatorClick;
+    customIndicators.appendChild(indicator);
+  }
+}
+
+function handleSlideChange() {
   var activeSlide = document.querySelector(".carousel-item.active");
   var slideIndex = Array.from(activeSlide.parentElement.children).indexOf(
     activeSlide
@@ -61,25 +74,24 @@ carousel.addEventListener("slid.bs.carousel", function () {
     indicator.classList.remove("active");
   });
 
-  // Menambah kelas active ke indikator yang sesuai dengan slide yang aktif
+  // Menambahkan kelas active ke indikator yang sesuai dengan slide yang aktif
   var activeIndicator = document.querySelector(
     `.custom-indicator[data-bs-slide-to="${slideIndex}"]`
   );
   activeIndicator.classList.add("active");
-});
-// function hidePreloader() {
-//   const preloader = document.getElementById("preloader");
-//   const content = document.getElementById("content");
-//   const nav = document.getElementById("nav");
-//   const footer = document.getElementById("footer");
-//   preloader.style.display = "none";
-//   content.style.display = "block";
-//   nav.style.display = "block";
-//   footer.style.display = "block";
-// }
+}
 
-// // Event listener untuk mendeteksi saat halaman telah dimuat sepenuhnya
-// window.addEventListener("load", () => {
-//   // Tunda pemanggilan hidePreloader selama minimal 3 detik (3000 milidetik)
-//   setTimeout(hidePreloader, 3000);
-// });
+function handleIndicatorClick() {
+  var slideNumber = this.getAttribute("data-bs-slide-to");
+  var carousel = document.getElementById("carouselExampleIndicators");
+  var carouselInstance = new bootstrap.Carousel(carousel);
+  carouselInstance.to(parseInt(slideNumber));
+}
+
+function showPreloader() {
+  $("#preloader").fadeIn();
+}
+
+function hidePreloader() {
+  $("#preloader").fadeOut();
+}
