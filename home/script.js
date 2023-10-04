6872516e9a012f408375ca8bebbfaaecbadd1b86
Thahirudin <tohiruzain098@gmail.com>
2023-10-04 15:02:31 +0700

    update home

--- home/script.js
+++ home/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  // Inisialisasi Carousel
+  // Inisialisasi Carousel Ulasan
   var owl = initializeCarousel();
 
   // Menghubungkan tombol prev dan next ke slider
@@ -15,7 +15,7 @@ $(document).ready(function () {
   var totalSlides = getTotalSlides();
   createCustomIndicators(totalSlides);
 
-  // Mendeteksi perubahan slide
+  // Mendeteksi perubahan slide carousel
   var carousel = document.getElementById("carouselExampleIndicators");
   carousel.addEventListener("slid.bs.carousel", handleSlideChange);
 
@@ -24,6 +24,40 @@ $(document).ready(function () {
 
   // Setelah 3 detik, sembunyikan preloader
   setTimeout(hidePreloader, 1500);
+  if ($(".bbb_slider").length) {
+    var trendsSlider = $(".bbb_slider");
+    trendsSlider.owlCarousel({
+      loop: false,
+      margin: 30,
+      nav: false,
+      dots: false,
+      autoplayHoverPause: true,
+      autoplay: false,
+      responsive: {
+        0: { items: 1 },
+        575: { items: 2 },
+        991: { items: 3 },
+      },
+    });
+
+    trendsSlider.on("click", ".bbb_fav", function (ev) {
+      $(ev.target).toggleClass("active");
+    });
+
+    if ($(".bbb_prev").length) {
+      var prev = $(".bbb_prev");
+      prev.on("click", function () {
+        trendsSlider.trigger("prev.owl.carousel");
+      });
+    }
+
+    if ($(".bbb_next").length) {
+      var next = $(".bbb_next");
+      next.on("click", function () {
+        trendsSlider.trigger("next.owl.carousel");
+      });
+    }
+  }
 });
 
 function initializeCarousel() {
