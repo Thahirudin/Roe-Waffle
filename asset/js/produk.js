f1e68140a7a27305eefa82906bfa00228ffbc3c0
Thahirudin <tohiruzain098@gmail.com>
2023-10-08 12:40:48 +0700

    perbaikan keseluruhan

--- asset/js/produk.js
+++ asset/js/produk.js
@@ -1,20 +1,8 @@
-function openInputMobile() {
-    var searchInputMobile = document.getElementById("search-input-mobile");
-    if (searchInputMobile.style.display === "none" || searchInputMobile.style.display === "") {
-      searchInputMobile.style.display = "block";
-    } else {
-      searchInputMobile.style.display = "none";
-    }
-}
-
-function searchMobile() {
-    var keyword = document.getElementById("search-bar-mobile").value;
-    window.location.href = "search.html?keyword=" + keyword;
-}
 // init Isotope
 var $grid = $('.collection-list').isotope({
   // options
 });
+
 // filter items on button click
 $('.filter-button-group').on( 'click', 'button', function() {
   var filterValue = $(this).attr('data-filter');
